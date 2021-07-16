21c4b5637bd4d587d9b1c1e648746739efbc326e
Mae Cagadas <32762531+rogueassassin11@users.noreply.github.com>
2021-07-16 22:45:21 +0800

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,10 @@
+/***************************************/
+/* FIRST STEPS
+
+DYNAMIC WEBSITE -> Website files are assembled on the server
+HTML -> Content
+CSS -> Presentation
+JS -> Dynamic and interactive effects
+
+index.html -> default name of the first page of a website
+/***************************************/
